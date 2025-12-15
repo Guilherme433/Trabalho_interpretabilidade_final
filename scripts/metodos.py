21a2ced95559848acead80739b0d288e3599c09e
Guilherme433 <121022106+Guilherme433@users.noreply.github.com>
2025-12-15 00:34:06 +0000

    Update metodos.py

--- scripts/metodos.py
+++ scripts/metodos.py
@@ -2,11 +2,11 @@
 from captum.attr import Saliency, IntegratedGradients, Occlusion, LayerGradCam, NoiseTunnel, GradientShap
 
 def get_methods_dict(model):
-    """Retorna o dicionário com os 5 métodos instanciados"""
     return {
         "Integrated Gradients": IntegratedGradients(model),
         "GradCAM": LayerGradCam(model, model.layer4),
         "Occlusion": Occlusion(model),
         "GradientSHAP": GradientShap(model),
         "SmoothGrad": NoiseTunnel(Saliency(model)) 
+
     }
